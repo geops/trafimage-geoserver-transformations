64253d7f322340cdc1f9cb86857ac172abf7ca90
Nico Mandery <nico.mandery@geops.de>
2014-08-27 14:45:18 +0200

    initial project setup [refs #14080071]

---
+++ src/main/java/org/geoserver/trafimage/transform/AggregateFeature.java
@@ -0,0 +1,16 @@
+package org.geoserver.trafimage.transform;
+
+import org.geoserver.wps.gs.GeoServerProcess;
+
+/*
+ * Documentation for Geoserver 2.4.x:
+ *    http://docs.geoserver.org/2.4.x/en/developer/programming-guide/wps-services/implementing.html
+ */
+
+public class AggregateFeature implements GeoServerProcess  {
+
+	public AggregateFeature() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
